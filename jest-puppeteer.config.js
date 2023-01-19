286bcaff6a88d2ce56c3833a416fbc2e26341fdb
arnasbastys <arnas.bastys@gmail.com>
2023-01-19 15:21:55 +0200

    Exec chrome path update13

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,6 @@ module.exports = {
   launch: {
     headless: false,
     args: ["--window-size=1920,1080"],
-    executablePath: "/opt/hostedtoolcache/chromium/latest/x64/chrome",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
   product: "chromium",
