749af663fbd7ac37934c0570823a423767826e29
arnasbastys <arnas.bastys@gmail.com>
2023-01-24 15:10:58 +0200

    Docker compose test 11

--- support/constants.ts
+++ support/constants.ts
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker";
 
 export const baseUrl =
   process.env.docker_running === "true"
-    ? `http://juice_shop_app:3000"`
+    ? `http://puppeteer-project-functional-pom-juice_shop_app-1:3000"`
     : `http://localhost:3000`;
 
 export const user = {
